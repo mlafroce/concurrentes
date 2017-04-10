8c0d45ffa4f02753221e5143662511039e4daa82
tbert <tomasbert12@gmail.com>
2017-04-10 02:10:32 -0300

    Wrapper para Memoria compartida. closes #11

--- tp-grupal-01/main.cpp
+++ tp-grupal-01/main.cpp
@@ -9,26 +9,25 @@ int getOptions(int argc, char** argv);
 
 int main(int argc, char** argv) {
 	int numPlayers = getOptions(argc, argv);
-	if (numPlayers < 0) {
-		return numPlayers;
-	}
-	DaringGame game(numPlayers);
+
+    DaringGame game(numPlayers);
 	SIGINT_Handler sigIntHandler(game);
-	SignalHandler::getInstance()->registerHandler(2, &sigIntHandler);
-	game.start();
-	SignalHandler::destruir();
+	SignalHandler::getInstance()->registerHandler(SIGINT, &sigIntHandler);
+
+    game.start();
+
+    SignalHandler::destruir();
 }
 
 int getOptions(int argc, char** argv) {
 	if(argc < 2) {
-		printf("Para iniciar el juego, debe indicar la cantidad de jugadores \
-por parámetro\n");
-		return -1;
+		printf("Para iniciar el juego, debe indicar la cantidad de jugadores por parámetro\n");
+		exit(-1);
 	}
 	int numPlayers = atoi(argv[1]);
 	if (numPlayers < 4 || numPlayers % 2){
 		printf("El número de jugadores debe ser par y mayor o igual a 4\n");
-		return -1;
+		exit(-1);
 	}
 	return numPlayers;
 }
--- tp-grupal-01/threaded-version.cpp
+++ tp-grupal-01/threaded-version.cpp
@@ -12,23 +12,25 @@ int main(int argc, char** argv) {
 	if (numPlayers < 0) {
 		return numPlayers;
 	}
+
 	DaringGame game(numPlayers);
 	SIGINT_Handler sigIntHandler(game);
-	SignalHandler::getInstance()->registerHandler(2, &sigIntHandler);
+	SignalHandler::getInstance()->registerHandler(SIGINT, &sigIntHandler);
+
 	game.start();
+
 	SignalHandler::destruir();
 }
 
 int getOptions(int argc, char** argv) {
-	if(argc < 2) {
-		printf("Para iniciar el juego, debe indicar la cantidad de jugadores \
-por parámetro\n");
-		return -1;
-	}
-	int numPlayers = atoi(argv[1]);
-	if (numPlayers < 4 || numPlayers % 2){
-		printf("El número de jugadores debe ser par y mayor o igual a 4\n");
-		return -1;
-	}
-	return numPlayers;
+    if(argc < 2) {
+        printf("Para iniciar el juego, debe indicar la cantidad de jugadores por parámetro\n");
+        exit(-1);
+    }
+    int numPlayers = atoi(argv[1]);
+    if (numPlayers < 4 || numPlayers % 2){
+        printf("El número de jugadores debe ser par y mayor o igual a 4\n");
+        exit(-1);
+    }
+    return numPlayers;
 }
