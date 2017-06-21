31d883fd72225dc8478867c82eedec32bd5e5c1a
Barba <mlafroce@gmail.com>
2017-06-21 03:54:03 -0300

    Agrego script para probar

---
+++ tp-grupal-02/test/test.sh
@@ -0,0 +1,17 @@
+./db-server &
+SERVER_PID=$!
+./db-client < client1.in &
+CLIENT1_PID=$!
+./db-client < client2.in &
+CLIENT2_PID=$!
+./db-client < client3.in &
+CLIENT3_PID=$!
+
+sleep 1
+
+kill -2 $CLIENT1_PID
+kill -2 $CLIENT2_PID
+kill -2 $CLIENT3_PID
+
+kill -2 $SERVER_PID
+
